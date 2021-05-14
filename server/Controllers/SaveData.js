09a274d02b02d026d06ea7729d6af1c50bf46abf
Dennis van der Waag <dennisvanderwaag@gmail.com>
2021-05-14 08:24:36 +0200

    Close mongoDB client after save

--- server/Controllers/SaveData.js
+++ server/Controllers/SaveData.js
@@ -156,7 +156,8 @@ const SaveData = () => {
     } catch(err) {
       console.error(err);
     } finally {
-        console.log('Cronjon done');
+      client.close();
+      console.log('Cronjon done');
     }
   });
 }
